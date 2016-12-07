f826d91e27838b5fad5a94033201dd692ce972fd
raghuadimulam <raghu.adimulam@digital.hmrc.gov.uk>
2016-12-07 13:41:04 +0000

    [LISA-40] - adding initial structure with RAML and scala-style

---
+++ project/MicroServiceBuild.scala
@@ -0,0 +1,72 @@
+import sbt._
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object MicroServiceBuild extends Build with MicroService {
+
+  val appName = "lisa-api"
+
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+}
+
+private object AppDependencies {
+  import play.sbt.PlayImport._
+  import play.core.PlayVersion
+
+  private val microserviceBootstrapVersion = "5.8.0"
+  private val playAuthVersion = "4.2.0"
+  private val playHealthVersion = "2.0.0"
+  private val logbackJsonLoggerVersion = "3.1.0"
+  private val playUrlBindersVersion = "2.0.0"
+  private val playConfigVersion = "3.0.0"
+  private val domainVersion = "4.0.0"
+  private val hmrcTestVersion = "2.2.0"
+  private val scalaTestVersion = "2.2.6"
+  private val pegdownVersion = "1.6.0"
+
+
+  val compile = Seq(
+
+    ws,
+    "uk.gov.hmrc" %% "microservice-bootstrap" % microserviceBootstrapVersion,
+    "uk.gov.hmrc" %% "play-authorisation" % playAuthVersion,
+    "uk.gov.hmrc" %% "play-health" % playHealthVersion,
+    "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
+    "uk.gov.hmrc" %% "play-config" % playConfigVersion,
+    "uk.gov.hmrc" %% "logback-json-logger" % logbackJsonLoggerVersion,
+    "uk.gov.hmrc" %% "domain" % domainVersion
+  )
+
+  trait TestDependencies {
+    lazy val scope: String = "test"
+    lazy val test : Seq[ModuleID] = ???
+  }
+
+  object Test {
+    def apply() = new TestDependencies {
+      override lazy val test = Seq(
+        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
+        "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
+        "org.pegdown" % "pegdown" % pegdownVersion % scope,
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+      )
+    }.test
+  }
+
+  object IntegrationTest {
+    def apply() = new TestDependencies {
+
+      override lazy val scope: String = "it"
+
+      override lazy val test = Seq(
+        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
+        "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
+        "org.pegdown" % "pegdown" % pegdownVersion % scope,
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+      )
+    }.test
+  }
+
+  def apply() = compile ++ Test() ++ IntegrationTest()
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,16 @@
+resolvers += Resolver.url("hmrc-sbt-plugin-releases", url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
+
+resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
+
+resolvers += Resolver.url("scoverage-bintray", url("https://dl.bintray.com/sksamuel/sbt-plugins/"))(Resolver.ivyStylePatterns)
+addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "1.3.0")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.4.0")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.9.0")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.0.0")
+
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.8")
+
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
