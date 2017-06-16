69bdccde5e77bf5343ac5182ddc9b06457cca153
Mark Reynolds <mark.reynolds@digital.hmrc.gov.uk>
2017-06-16 11:55:29 +0100

    Remove commented out code from MicroserviceGlobal

--- app/uk/gov/hmrc/lisaapi/config/microserviceGlobal.scala
+++ app/uk/gov/hmrc/lisaapi/config/microserviceGlobal.scala
@@ -63,12 +63,6 @@ object MicroserviceLoggingFilter extends LoggingFilter with MicroserviceFilterSu
   override def controllerNeedsLogging(controllerName: String) = ControllerConfiguration.paramsForController(controllerName).needsLogging
 }
 
-//object MicroserviceAuthFilter extends AuthorisationFilter with MicroserviceFilterSupport {
-//  override lazy val authParamsConfig = AuthParamsControllerConfiguration
-//  override lazy val authConnector = MicroserviceAuthConnector
-//  override def controllerNeedsAuth(controllerName: String): Boolean = ControllerConfiguration.paramsForController(controllerName).needsAuth
-//}
-
 object MicroserviceGlobal extends DefaultMicroserviceGlobal with RunMode with MicroserviceFilterSupport with ServiceLocatorRegistration {
   override val auditConnector = MicroserviceAuditConnector
 
