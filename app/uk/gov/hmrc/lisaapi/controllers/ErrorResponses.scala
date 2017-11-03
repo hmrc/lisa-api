3ff2d5be6240c753a89e5da47e90e3520992b2f8
anand.prakash <anand.prakash@digital.hmrc.gov.uk>
2017-11-03 12:02:05 +0000

    Updated the content of the error messages.

--- app/uk/gov/hmrc/lisaapi/controllers/ErrorResponses.scala
+++ app/uk/gov/hmrc/lisaapi/controllers/ErrorResponses.scala
@@ -90,11 +90,11 @@ case object ErrorInvestorNotEligible extends ErrorResponse(403, "INVESTOR_ELIGIB
 
 case object ErrorInvestorComplianceCheckFailed extends ErrorResponse(403, "INVESTOR_COMPLIANCE_CHECK_FAILED", "The investor has failed a compliance check - they may have breached ISA guidelines or regulations")
 
-case object ErrorAccountCancellationPeriodExceeded extends ErrorResponse(403, "CANCELLATION_PERIOD_EXCEEDED", "The LISA account cancellation period exceeded")
+case object ErrorAccountCancellationPeriodExceeded extends ErrorResponse(403, "CANCELLATION_PERIOD_EXCEEDED", "The account cannot be closed with a closure reason of 'Cancellation' as the cancellation period has been exceeded")
 
-case object ErrorAccountWithinCancellationPeriod extends ErrorResponse(403, "ACCOUNT_WITHIN_CANCELLATION_PERIOD", "The LISA account within cancellation period")
+case object ErrorAccountWithinCancellationPeriod extends ErrorResponse(403, "ACCOUNT_WITHIN_CANCELLATION_PERIOD", "The account cannot be closed with a closure reason of 'All funds withdrawn' as this  is still within the cancellation period")
 
-case object ErrorAccountBonusPaymentRequired extends ErrorResponse(403, "BONUS_REPAYMENT_REQUIRED", "The LISA account bonus payment required")
+case object ErrorAccountBonusPaymentRequired extends ErrorResponse(403, "BONUS_REPAYMENT_REQUIRED", "The account cannot be closed with a closure reason of 'Cancellation' as a bonus payment needs to be repaid")
 
 case object ErrorPreviousAccountDoesNotExist extends ErrorResponse(403, "PREVIOUS_INVESTOR_ACCOUNT_DOES_NOT_EXIST", "The transferredFromAccountId and transferredFromLMRN given don’t match with an account on HMRC’s records")
 
