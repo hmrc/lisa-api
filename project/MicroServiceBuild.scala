0ff623c121b89bbb537a7f8b4af6e16133956b87
Chris Waugh <mail@chriswaugh.net>
2018-07-16 10:44:16 +0100

    Updated microservice bootstrap

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -14,7 +14,7 @@ private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
-  private val microserviceBootstrapVersion = "6.13.0"
+  private val microserviceBootstrapVersion = "6.18.0"
   private val domainVersion = "5.0.0"
   private val authclientVersion = "2.4.0"
   private val scalaTestVersion = "2.2.6"
