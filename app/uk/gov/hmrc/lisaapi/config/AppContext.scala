ac00f1b20a0983c400dd45a038d9e223e993d961
Drew Miley <drew.miley@opencastsoftware.com>
2019-03-13 15:57:18 +0000

    LISA-2416: Fixes app context

--- app/uk/gov/hmrc/lisaapi/config/AppContext.scala
+++ app/uk/gov/hmrc/lisaapi/config/AppContext.scala
@@ -16,15 +16,12 @@
 
 package uk.gov.hmrc.lisaapi.config
 
-import com.google.inject.Inject
-import play.api.{Configuration, Environment}
-import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
-import uk.gov.hmrc.play.config.ServicesConfig
+import javax.inject.{Inject, Singleton}
+import play.api.Configuration
+import uk.gov.hmrc.play.bootstrap.config.{RunMode, ServicesConfig}
 
-class AppContext @Inject()(
-                            val runModeConfiguration: Configuration,
-                            environment: Environment
-                          ) extends ServicesConfig {
+@Singleton
+class AppContext @Inject()(runModeConfiguration: Configuration, runMode: RunMode) extends ServicesConfig(runModeConfiguration: Configuration, runMode: RunMode) {
   lazy val appName = runModeConfiguration.getString("appName").getOrElse(throw new RuntimeException("appName is not configured"))
   lazy val appUrl = runModeConfiguration.getString("appUrl").getOrElse(throw new RuntimeException("appUrl is not configured"))
   lazy val serviceLocatorUrl: String = baseUrl("service-locator")
@@ -38,8 +35,6 @@ class AppContext @Inject()(
   lazy val access = runModeConfiguration.getConfig(s"api.access")
   lazy val v1endpointsEnabled = runModeConfiguration.getBoolean("api.endpointsEnabled").getOrElse(throw new RuntimeException(s"Missing key api.endpointsEnabled"))
   lazy val v2endpointsEnabled = runModeConfiguration.getBoolean("api.endpointsEnabledv2").getOrElse(throw new RuntimeException(s"Missing key api.endpointsEnabledv2"))
-
-  override protected def mode = environment.mode
 
   def endpointIsDisabled(endpoint: String): Boolean = {
     runModeConfiguration.getStringList("api.disabledEndpoints").fold(false)(list => list.contains(endpoint))
