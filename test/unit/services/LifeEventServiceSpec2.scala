ff86d680db38f6e815057571ea94fd0668872f70
danewatts <danewatts97@gmail.com>
2019-07-01 14:15:07 +0100

    WIP

--- test/unit/services/LifeEventServiceSpec2.scala
+++ test/unit/services/LifeEventServiceSpec2.scala
@@ -22,29 +22,23 @@ import play.api.test.Injecting
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.lisaapi.connectors.DesConnector
 import uk.gov.hmrc.lisaapi.models.ReportLifeEventRequest
-import uk.gov.hmrc.lisaapi.models.des.DesResponse
+import uk.gov.hmrc.lisaapi.models.des.{DesFailureResponse, DesResponse}
 import unit.utils.{DesWireMockConnector, WireMockHelper}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-class LifeEventServiceSpec2 extends WireMockHelper with Injecting {
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+class LifeEventServiceSpec2 extends DesWireMockConnector with Injecting {
 
   "LiveEventController " must {
     "return ReportLifeEventAccountClosedResponse" when {
       "the error code is INVESTOR_ACCOUNT_ALREADY_CLOSED" in {
 
-        server.stubFor(post(urlPathEqualTo("/lifetime-isa/manager/Z019283/accounts/192837/life-event"))
-          .willReturn(aResponse()
-            .withStatus(403)
-            .withBody("INVESTOR_ACCOUNT_ALREADY_CLOSED")
-          )
-        )
-        //val responseFuture : Future[DesResponse] = DesConnector2.reportLifeEvent("Z019283", "192837", ReportLifeEventRequest("LISA Investor Terminal Ill Health", new DateTime("2017-04-06")))(HeaderCarrier())
-        val responseFuture: Future[DesResponse] = inject[DesConnector].reportLifeEvent("Z019283", "192837", ReportLifeEventRequest("LISA Investor Terminal Ill Health", new DateTime("2017-04-06")))(HeaderCarrier())
+        desWireMockConnectorStub("/lifetime-isa/manager/Z019283/accounts/192837/life-event", "INVESTOR_ACCOUNT_ALREADY_CLOSED", 403)
+        
+        val responseFuture : Future[DesResponse] = DesConnector2.reportLifeEvent("Z019283", "192837", ReportLifeEventRequest("LISA Investor Terminal Ill Health", new DateTime("2017-04-06")))(HeaderCarrier())
 
-        responseFuture.onComplete(
-          resp => println("FFFFFFFFFFF"+resp)
-        )
+        Await.result(responseFuture, Duration.Inf) mustBe DesFailureResponse("INVESTOR_ACCOUNT_ALREADY_CLOSED","")
 
 //        responseFuture mustBe Future.successful(DesFailureResponse("INVESTOR_ACCOUNT_ALREADY_CLOSED",""))
 //
