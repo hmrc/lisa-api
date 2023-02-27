8bb1a02d3c063cbf003a92b38194a461d8d85041
slavvco <13876870+slavvco@users.noreply.github.com>
2023-02-27 14:02:55 +0000

    DDCE-3468 | Updated dependencies.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Requirements
 
-This service is written in [Scala 2.12](http://www.scala-lang.org/) and [Play 2.7](http://playframework.com/), so needs at least a [JRE 1.8](http://www.oracle.com/technetwork/java/javase/downloads/index.html) to run.
+This service is written in [Scala 2.12](http://www.scala-lang.org/) and [Play](http://playframework.com/), so needs at least a [JRE 1.8](http://www.oracle.com/technetwork/java/javase/downloads/index.html) to run.
 
 ## Description
 
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,24 +6,26 @@ import sbt._
 object AppDependencies {
 
   val silencerVersion = "1.7.1"
+  val bootstrapVersion = "7.14.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"        %% "bootstrap-backend-play-28"  % "5.24.0",
-    "uk.gov.hmrc"        %% "play-hmrc-api"              % "6.4.0-play-28",
-    "com.typesafe.play"  %% "play-json-joda"             % "2.9.2",
+    "uk.gov.hmrc"        %% "bootstrap-backend-play-28"  % bootstrapVersion,
+    "uk.gov.hmrc"        %% "play-hmrc-api"              % "7.1.0-play-28",
+    "com.typesafe.play"  %% "play-json-joda"             % "2.9.4",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % silencerVersion % Provided cross CrossVersion.full
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %%  "scalatest"          %  "3.2.9",
-    "org.scalatestplus"       %%  "mockito-3-4"        %  "3.2.9.0",
-    "org.scalatestplus.play"  %%  "scalatestplus-play" %  "5.1.0",
-    "com.typesafe.play"       %%  "play-test"          %  PlayVersion.current,
-    "org.scalaj"              %%  "scalaj-http"        %  "2.4.2",
-    "org.pegdown"             %   "pegdown"            %  "1.6.0",
-    "com.vladsch.flexmark"    %   "flexmark-all"       %  "0.36.8"
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"  % bootstrapVersion,
+    "org.scalatest"           %%  "scalatest"              %  "3.2.9",
+    "org.scalatestplus"       %%  "mockito-3-4"            %  "3.2.9.0",
+    "org.scalatestplus.play"  %%  "scalatestplus-play"     %  "5.1.0",
+    "com.typesafe.play"       %%  "play-test"              %  PlayVersion.current,
+    "org.scalaj"              %%  "scalaj-http"            %  "2.4.2",
+    "org.pegdown"             %   "pegdown"                %  "1.6.0",
+    "com.vladsch.flexmark"    %   "flexmark-all"           %  "0.36.8"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
