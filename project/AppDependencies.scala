df0e39da8b4fdbe0dc8a87de702ca26f58a53e02
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2024-04-16 12:45:42 +0100

    DDCE-5024: Fix for failing tests and tidy up dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,12 +13,8 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.scalatest"          %% "scalatest"              % "3.2.18",
     "org.scalatestplus"      %% "mockito-3-4"            % "3.2.10.0",
-    "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.1",
-    "org.scalaj"             %% "scalaj-http"            % "2.4.2",
-    "org.pegdown"             % "pegdown"                % "1.6.0",
-    "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.8"
+    "org.scalaj"             %% "scalaj-http"            % "2.4.2"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
