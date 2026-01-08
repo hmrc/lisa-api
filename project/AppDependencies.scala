93fb731044682c21c0e99d056853bf2a731c0bfa
maheshhvr <175945803+maheshhvr@users.noreply.github.com>
2026-01-08 14:43:55 +0000

    [LISA]: Align License Headers for LISA Service Repos Deps update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.4.0"
+  val bootstrapVersion = "10.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %%  "bootstrap-backend-play-30"   % bootstrapVersion,
